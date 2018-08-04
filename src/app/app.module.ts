5c6f1c9d552a915859f55306274aac589637408b
Administrator <Administrator@ALIENDEVVM>
2018-08-04 17:08:35 -0400

    commiting changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductService } from './product.service';
+import { ProductTrackinglistComponent } from './product-trackinglist/product-trackinglist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
-    ProductDescriptionComponent
+    ProductDescriptionComponent,
+    ProductTrackinglistComponent
   ],
   imports: [
     BrowserModule,
