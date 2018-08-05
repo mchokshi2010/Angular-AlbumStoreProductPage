251815fbecf4ebb19c0e1e46a9c1a38f39e173fb
Administrator <Administrator@ALIENDEVVM>
2018-08-05 13:07:10 -0400

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,13 @@ import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductService } from './product.service';
-import { ProductTrackListingComponent } from './product-tracklisting/product-tracklisting.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
-    ProductDescriptionComponent,
-    ProductTrackListingComponent
+    ProductDescriptionComponent
   ],
   imports: [
     BrowserModule,
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/observable';
 
-@Injectable()
+//@Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
