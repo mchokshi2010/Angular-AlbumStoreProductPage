61ff11e7730e8a760341e5b18a87ec8e361da070
Administrator <Administrator@ALIENDEVVM>
2018-08-05 13:36:47 -0400

    updated product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-
 import 'rxjs/add/operator/map';
 import { Album } from './album';
-import { Observable } from '../../node_modules/rxjs/observable';
+import { Observable } from 'rxjs/observable';
 
 //@Injectable()
 export class ProductService {
